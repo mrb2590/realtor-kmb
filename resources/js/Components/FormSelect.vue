23688bf7f61e49de3c53ad0d2ae14d90bba97daf
Mike Buonomo <mrb2590@gmail.com>
2024-01-14 17:57:57 -0500

    30-Theme-Vue-Components (#32)

--- resources/js/Components/FormSelect.vue
+++ resources/js/Components/FormSelect.vue
@@ -27,7 +27,7 @@
 
 <template>
   <select
-    class="rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-indigo-600 dark:focus:ring-indigo-600"
+    class="rounded-md border-gray-300 shadow-sm focus:border-primary-500 focus:ring-primary-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-primary-600 dark:focus:ring-primary-600"
     :value="modelValue"
     @input="$emit('update:modelValue', $event.target.value)"
     ref="select"
