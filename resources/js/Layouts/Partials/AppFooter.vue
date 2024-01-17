ffa8069a03f913a48eb1813b40478d974391fe69
Mike Buonomo <mrb2590@gmail.com>
2024-01-16 22:29:50 -0500

    37-extract-navs-to-component (#38)

--- resources/js/Layouts/Partials/AppFooter.vue
+++ resources/js/Layouts/Partials/AppFooter.vue
@@ -1,7 +1,14 @@
 <script setup>
   import ApplicationLogo from '@/Components/ApplicationLogo.vue';
   import AppLink from '@/Components/AppLink.vue';
-  import { navLinks } from '@/Data/NavLinks';
+  import { links } from '@/Data/Links';
+
+  const navLinks = {
+    about: { ...links.about },
+    contact: { ...links.contact },
+    services: { ...links.services },
+    social: { ...links.social }
+  };
 </script>
 
 <template>
@@ -35,13 +42,13 @@
         <div
           class="flex w-full max-w-sm flex-row flex-wrap items-start justify-between md:max-w-full"
         >
-          <div class="w-auto min-w-40 max-w-52 p-3" v-for="(navLink, i) in navLinks" :key="i">
+          <!-- Nav Links -->
+          <div class="w-auto min-w-40 max-w-52 p-3" v-for="navLink in navLinks">
             <h3 class="mb-2 text-xl font-bold">{{ navLink.title }}</h3>
             <ul>
               <li
-                class="mb-1 flex flex-row space-x-2 text-sm"
-                v-for="(link, j) in navLink.links"
-                :key="j"
+                class="mb-1 flex flex-row items-center space-x-2 text-sm"
+                v-for="link in navLink.subLinks"
               >
                 <component
                   :is="link.icon.component"
@@ -49,7 +56,7 @@
                   class="h-auto w-4"
                   :class="link.icon.classes"
                 />
-                <AppLink :href="link.href" class="" :router="link.router">
+                <AppLink :href="link.href" :router="link.router">
                   {{ link.title }}
                 </AppLink>
               </li>
---
+++ resources/js/Layouts/Partials/AppTopNav.vue
@@ -0,0 +1,49 @@
+<script setup>
+  import AppTopNavLink from '@/Components/AppTopNavLink.vue';
+  import { links } from '@/Data/Links';
+</script>
+
+<template>
+  <nav class="z-20 w-full bg-primary-800 font-light">
+    <div
+      class="container mx-auto flex w-full flex-row items-center justify-between space-x-6 px-4 py-2"
+    >
+      <ul class="flex w-full items-center space-x-6">
+        <li
+          class="flex flex-row items-center space-x-2 text-sm text-white"
+          v-for="link in links.contact.subLinks"
+        >
+          <component
+            :is="link.icon.component"
+            v-if="link.icon"
+            class="h-auto w-4"
+            :class="link.icon.classes"
+          />
+          <AppTopNavLink :href="link.href" :router="link.router">
+            {{ link.title }}
+          </AppTopNavLink>
+        </li>
+      </ul>
+      <ul class="flex w-full basis-0 items-center space-x-2">
+        <li
+          class="flex flex-row items-center text-sm text-white"
+          v-for="link in links.social.subLinks"
+        >
+          <AppTopNavLink
+            :href="link.href"
+            :router="link.router"
+            :title="link.title"
+            class="rounded-full bg-white p-1"
+          >
+            <component
+              :is="link.icon.component"
+              v-if="link.icon"
+              class="h-auto w-4"
+              :class="link.icon.classes"
+            />
+          </AppTopNavLink>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
